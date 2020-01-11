33b3111a1df80963de7cfa8eb7e0806312ccd53a
fieldin0831 <field_in@nike.eonet.ne.jp>
2020-01-11 22:31:06 +0900

    initial project commit

---
+++ PrintRubyTests/PrintRubyTests.swift
@@ -0,0 +1,34 @@
+//
+//  PrintRubyTests.swift
+//  PrintRubyTests
+//
+//  Created by Yuki Hatanaka on 2020/01/11.
+//  Copyright © 2020 fieldin0831. All rights reserved.
+//
+
+import XCTest
+@testable import PrintRuby
+
+class PrintRubyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
