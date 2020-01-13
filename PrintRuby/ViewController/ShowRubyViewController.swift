f0bfb744ab1b8cd2f350b6a34737bcaf9acb912c
fieldin0831 <field_in@nike.eonet.ne.jp>
2020-01-13 16:29:41 +0900

    ルビ表示画面対応

--- PrintRuby/ViewController/ShowRubyViewController.swift
+++ PrintRuby/ViewController/ShowRubyViewController.swift
@@ -8,26 +8,87 @@
 
 import UIKit
 
-class ShowRubyViewController: UIViewController {
+/// ルビ表示画面ViewControllerクラス
+class ShowRubyViewController: BaseViewController {
 
-    @IBOutlet weak var sourceWordTextView: CustomTextView!
-    @IBOutlet weak var destinationWordLabel: CustomTextView!
+    @IBOutlet weak var sourceStringTextView: CustomTextView!
+    @IBOutlet weak var destinationStringTextView: CustomTextView!
+    @IBOutlet weak var speakButton: CustomButton!
+    
+    /// Viewモデル
+    let viewModel: ShowRubyViewModel = ShowRubyViewModel()
+    
+    
+    // MARK: - override function
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // 初期化
+        self.initViewController()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        self.viewModel.stopSpeaking()
     }
-    */
-
+    
+    override func setViewData(_ viewData: BaseViewData) {
+        // ルビ表示画面データならそれを設定
+        if viewData is ShowRubyViewData {
+            self.viewModel.setViewData(viewData as! ShowRubyViewData)
+        }
+    }
+    
+    
+    // MARK: - private function
+    
+    /// 初期化
+    private func initViewController() {
+        // Viewモデル初期化
+        self.viewModel.initViewModel()
+        
+        // 表示テキスト設定
+        self.sourceStringTextView.text = self.viewModel.viewData.sourceString
+        self.destinationStringTextView.text = self.viewModel.viewData.destinationString
+        
+        // 読み上げボタン状態更新
+        self.updateSpeakButtonState()
+    }
+    
+    /// 読み上げボタン状態更新
+    private func updateSpeakButtonState() {
+        // 読み上げ状態判定
+        if self.viewModel.isSpeaking {
+            // 読み上げ中なら停止ボタン
+            self.speakButton.setTitle("■", for: .normal)
+            self.speakButton.setImage(nil, for: .normal)
+        }
+        else {
+            // 停止中なら読み上げボタン
+            self.speakButton.setTitle("", for: .normal)
+            self.speakButton.setImage(R.image.speak(), for: .normal)
+        }
+    }
+    
+    
+    // MARK: - IBAction
+    
+    /// 読み上げボタンタップアクション
+    @IBAction func touchUpInsideSpeakButton(_ sender: UIButton) {
+        // 読み上げ状態判定
+        if self.viewModel.isSpeaking {
+            // 読み上げ中なら停止させる
+            self.viewModel.stopSpeaking()
+            self.updateSpeakButtonState()
+        }
+        else {
+            // 停止中なら読み上げ開始する
+            self.viewModel.speak(text: viewModel.viewData.destinationString) { [weak self] in
+                self?.updateSpeakButtonState()
+            }
+            self.updateSpeakButtonState()
+        }
+    }
 }
--- PrintRuby/ViewModel/ShowRubyViewModel.swift
+++ PrintRuby/ViewModel/ShowRubyViewModel.swift
@@ -7,7 +7,9 @@
 //
 
 import Foundation
+import AVFoundation
 
+/// ルビ表示画面データ
 class ShowRubyViewData: BaseViewData {
     /// 変換元文字列
     var sourceString: String = ""
@@ -15,10 +17,23 @@ class ShowRubyViewData: BaseViewData {
     var destinationString: String = ""
 }
 
+/// ルビ表示画面モデル
 class ShowRubyViewModel: NSObject {
     /// Viewデータ
     let viewData: ShowRubyViewData = ShowRubyViewData()
+    /// AVSpeechSynthesizer
+    let speechSynthesizer = AVSpeechSynthesizer()
+    /// 読み上げ完了クロージャ保持用
+    var speakingDidFinishHandler: (() -> Void)?
+    /// 読み上げ状態
+    var isSpeaking: Bool = false
     
+    // MARK: - internal function
+    
+    /// Viewモデル初期化
+    func initViewModel() {
+        self.speechSynthesizer.delegate = self
+    }
     
     /// Viewデータ設定
     ///
@@ -27,5 +42,54 @@ class ShowRubyViewModel: NSObject {
     func setViewData(_ viewData: ShowRubyViewData) {
         self.viewData.sourceString = viewData.sourceString
         self.viewData.destinationString = viewData.destinationString
+    }
+    
+    /// 読み上げ開始
+    ///
+    /// - Parameters:
+    ///   - text: 読み上げるテキスト
+    ///   - didFinish: 読み上げ完了クロージャ
+    func speak(text: String, didFinish: @escaping () -> Void) {
+        // 読み上げ停止
+        self.stopSpeaking()
+        
+        // 状態更新
+        self.isSpeaking = true
+        self.speakingDidFinishHandler = didFinish
+        
+        // 日本語で読み上げ開始
+        let utterance = AVSpeechUtterance(string: text)
+        utterance.voice = AVSpeechSynthesisVoice(language: "ja-JP")
+        self.speechSynthesizer.speak(utterance)
+    }
+    
+    /// 読み上げ停止
+    func stopSpeaking() {
+        // 読み上げ中なら停止する
+        if self.isSpeaking {
+            self.speechSynthesizer.stopSpeaking(at: .immediate)
+        }
+        
+        // 状態クリア
+        self.isSpeaking = false
+        self.speakingDidFinishHandler = nil
+    }
+}
+
+
+// MARK: - AVSpeechSynthesizerDelegate function
+
+extension ShowRubyViewModel: AVSpeechSynthesizerDelegate {
+    
+    /// 読み上げ完了通知
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+        // 状態クリア
+        self.isSpeaking = false
+        
+        // 完了クロージャ呼び出し
+        if self.speakingDidFinishHandler != nil {
+            self.speakingDidFinishHandler!()
+            self.speakingDidFinishHandler = nil
+        }
     }
 }
